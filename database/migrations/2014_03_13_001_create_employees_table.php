534aa303d879c577c97ba67d16fe7deba411a206
ISMAEL E. LASTRE ALVAREZ <ismaelfac@gmail.com>
2020-03-12 21:23:29 -0500

    Model Employee <-> Users

---
+++ database/migrations/2014_03_13_001_create_employees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('dni');
+            $table->string('lastName');
+            $table->string('firstName');
+            $table->boolean('gender')->default(1);//1.Masculino - 2.Femenino
+            $table->date('dateBirth');
+            $table->timestamps();
+            $table->softDeletesTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
